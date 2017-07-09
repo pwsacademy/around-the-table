b8f0aa46c352d8d9961c48605f9fdcfed336ff39
Steven Van Impe <steven.vanimpe@icloud.com>
2017-07-09 20:32:36 +0200

    Added support for deploying to a test server.

--- Sources/Server/Secrets.swift
+++ Sources/Server/Secrets.swift
@@ -5,15 +5,15 @@ import Configuration
  */
 enum Secrets {
     
-    private static let configuration = ConfigurationManager().load(file: "Configuration/secrets.json", relativeFrom: .project)
+    private static let secrets = ConfigurationManager().load(file: "Configuration/secrets.json", relativeFrom: .project)
     
     // Used to encrypt the session ID cookie.
-    static let sessionSecret = configuration["secrets:sessionSecret"] as! String
+    static let sessionSecret = secrets["secrets:sessionSecret"] as! String
     
     // Used to authenticate with Facebook for Web Login.
-    static let facebookAppID = configuration["secrets:facebookAppID"] as! String
-    static let facebookAppSecret = configuration["secrets:facebookAppSecret"] as! String
+    static let facebookAppID = secrets["secrets:facebookAppID"] as! String
+    static let facebookAppSecret = secrets["secrets:facebookAppSecret"] as! String
     
     // Used to authenticate with Google for Maps and Places.
-    static let googleAPIKey = configuration["secrets:googleAPIKey"] as! String
+    static let googleAPIKey = secrets["secrets:googleAPIKey"] as! String
 }
--- Sources/Server/Settings.swift
+++ Sources/Server/Settings.swift
@@ -6,42 +6,45 @@ import Foundation
  */
 enum Settings {
     
-    private static let configuration = ConfigurationManager().load(file: "Configuration/settings.json", relativeFrom: .project)
+    private static let settings = ConfigurationManager().load(file: "Configuration/settings.json", relativeFrom: .project)
     
     /*
      Configures a custom domain name. 
      If a custom domain name is present, the app will forward all requests to this custom domain.
      This forwarding is necessary as the app uses session cookies, which are attached to a particular domain.
      This setting does not affect how the app behaves when running on localhost.
+     It is also ignored when deploying to a test server.
      */
-    static let customDomainName = configuration["settings:customDomainName"] as? String
+    static let customDomainName = configuration["TEST"] == nil ? settings["settings:customDomainName"] as? String : nil
     
     // Default pictures.
-    static let defaultGamePicture = configuration["settings:defaultGamePicture"] as! String
-    static let defaultGameThumbnail = configuration["settings:defaultGameThumbnail"] as! String
-    static let defaultProfilePicture = configuration["settings:defaultProfilePicture"] as! String
+    static let defaultGamePicture = settings["settings:defaultGamePicture"] as! String
+    static let defaultGameThumbnail = settings["settings:defaultGameThumbnail"] as! String
+    static let defaultProfilePicture = settings["settings:defaultProfilePicture"] as! String
     
     // Used to constrain the results returned by the Google Places autocompletion field.
-    static let countries = configuration["settings:countries"] as! [String]
+    static let countries = settings["settings:countries"] as! [String]
     // Used to select the views to render and to localize anything that needs localization (e.g. dates).
-    static let locale = configuration["settings:locale"] as! String
+    static let locale = settings["settings:locale"] as! String
     // Used to display dates in the correct time zone.
-    static let timeZone = TimeZone(identifier: configuration["settings:timeZone"] as! String) ?? TimeZone.current
+    static let timeZone = TimeZone(identifier: settings["settings:timeZone"] as! String) ?? TimeZone.current
     
     // Used to calculate distances when the user has disabled geolocation.
     enum defaultCoordinates {
         
-        static let latitude = configuration["settings:defaultCoordinates:latitude"] as! Double
-        static let longitude = configuration["settings:defaultCoordinates:longitude"] as! Double
+        static let latitude = settings["settings:defaultCoordinates:latitude"] as! Double
+        static let longitude = settings["settings:defaultCoordinates:longitude"] as! Double
     }
     
     enum database {
         
         // The name by which the Compose for MongoDB service is connected to your Bluemix app.
-        static let bluemixService = configuration["settings:database:bluemixService"] as! String
+        static let bluemixService = settings["settings:database:bluemixService"] as! String
         // The URI of your MongoDB database when running on localhost.
-        static let localURI = configuration["settings:database:localURI"] as! String
-        // The name of your database.
-        static let name = configuration["settings:database:name"] as! String
+        static let localURI = settings["settings:database:localURI"] as! String
+        // The name of your database. When deploying to a test server, a test database is used.
+        static let name = configuration["TEST"] == nil ?
+            settings["settings:database:name"] as! String :
+            settings["settings:database:test"] as! String
     }
 }
