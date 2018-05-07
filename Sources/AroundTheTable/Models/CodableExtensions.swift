b10faa54e08517c0d6ff5a16b61d73b19ca99a52
Steven Van Impe <steven.vanimpe@icloud.com>
2018-05-07 20:46:12 +0200

    Import CodableExtensions.

---
+++ Sources/AroundTheTable/Models/CodableExtensions.swift
@@ -0,0 +1,25 @@
+/**
+ Adds `Codable` conformance to `CountableClosedRange`.
+ 
+ A `CountableClosedRange` is coded by coding the `lowerBound` and `upperBound` properties.
+ */
+extension CountableClosedRange: Codable where Bound: Codable {
+
+    enum CodingKeys: CodingKey {
+        case lowerBound
+        case upperBound
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(lowerBound, forKey: .lowerBound)
+        try container.encode(upperBound, forKey: .upperBound)
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let lowerBound = try container.decode(Bound.self, forKey: .lowerBound)
+        let upperBound = try container.decode(Bound.self, forKey: .upperBound)
+        self = lowerBound...upperBound
+    }
+}
