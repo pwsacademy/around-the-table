3139d3372aaa3ffee4ffdce5c2dbd7775ce32ccf
Steven Van Impe <steven.vanimpe@icloud.com>
2018-05-31 17:21:00 +0200

    Added web routes for the home page and FAQ.

--- Sources/AroundTheTable/Routes/WebRoutes.swift
+++ Sources/AroundTheTable/Routes/WebRoutes.swift
@@ -3,6 +3,29 @@ import Kitura
 
 extension Routes {
     
+    /**
+     Registers the web routes on the given router.
+     */
     func configureWeb(using router: Router, credentials: Credentials) {
+        router.get("home", handler: home)
+        router.get("faq", handler: faq)
+    }
+    
+    /**
+     Shows the home page.
+     */
+    private func home(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        let base = try baseViewModel(for: request)
+        try response.render("home", with: base, forKey: "base")
+        next()
+    }
+    
+    /**
+     Shows the FAQ.
+     */
+    private func faq(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        let base = try baseViewModel(for: request)
+        try response.render("faq", with: base, forKey: "base")
+        next()
     }
 }
