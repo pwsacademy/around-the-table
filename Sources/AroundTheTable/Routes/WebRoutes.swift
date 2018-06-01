d2bf4d6337e8ef579bb431fe82147e7466c31c15
Steven Van Impe <steven.vanimpe@icloud.com>
2018-06-01 09:18:29 +0200

    Added AuthenticationMiddleware and web routes for activities.

--- Sources/AroundTheTable/Routes/WebRoutes.swift
+++ Sources/AroundTheTable/Routes/WebRoutes.swift
@@ -9,6 +9,13 @@ extension Routes {
     func configureWeb(using router: Router, credentials: Credentials) {
         router.get("home", handler: home)
         router.get("faq", handler: faq)
+        
+        router.get("games", handler: activities)
+        router.get("newest-games", handler: newestActivities)
+        router.get("upcoming-games", handler: upcomingActivities)
+        router.get("games-near-me", handler: activitiesNearMe)
+        
+        let authentication = AuthenticationMiddleware(persistence: persistence)
     }
     
     /**
@@ -26,6 +33,130 @@ extension Routes {
     private func faq(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
         let base = try baseViewModel(for: request)
         try response.render("faq", with: base, forKey: "base")
+        next()
+    }
+    
+    /**
+     Shows a selection of new activities, upcoming activities and activities near the user.
+     */
+    private func activities(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        let user: User?
+        if let userID = request.userProfile?.id {
+            guard let existingUser = try persistence.user(withID: userID) else {
+                throw log(ServerError.missingMiddleware(type: AuthenticationMiddleware.self))
+            }
+            user = existingUser
+        } else {
+            user = nil
+        }
+        let coordinates = user?.location?.coordinates ?? .default
+        let newest = try persistence.newestActivities(notHostedBy: user, measuredFrom: coordinates, startingFrom: 0, limitedTo: 4)
+        let upcoming = try persistence.upcomingActivities(notHostedBy: user, measuredFrom: coordinates, startingFrom: 0, limitedTo: 4)
+        let closest = user?.location != nil ? try persistence.activitiesNear(user: user!, startingFrom: 0, limitedTo: 4) : []
+        let base = try baseViewModel(for: request)
+        try response.render("games", with: ActivitiesViewModel(base: base,
+                                                               newest: newest,
+                                                               upcoming: upcoming,
+                                                               closest: closest))
+        next()
+    }
+    
+    /**
+     Shows a paged list of activities, sorted by date of creation.
+     */
+    private func newestActivities(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        guard let pageString = request.queryParameters["page"],
+              let page = Int(pageString) else {
+            response.status(.badRequest)
+            return next()
+        }
+        let user: User?
+        if let userID = request.userProfile?.id {
+            guard let existingUser = try persistence.user(withID: userID) else {
+                throw log(ServerError.missingMiddleware(type: AuthenticationMiddleware.self))
+            }
+            user = existingUser
+        } else {
+            user = nil
+        }
+        let coordinates = user?.location?.coordinates ?? .default
+        let pageSize = 8
+        let activities = try persistence.newestActivities(notHostedBy: user,
+                                                          measuredFrom: coordinates,
+                                                          startingFrom: page * pageSize,
+                                                          limitedTo: pageSize)
+        let remaining = try persistence.numberOfActivities(notHostedBy: user) - (page + 1) * pageSize
+        let base = try baseViewModel(for: request)
+        try response.render("games-page", with: ActivitiesPageViewModel(base: base,
+                                                                        type: .newest,
+                                                                        activities: activities,
+                                                                        page: page,
+                                                                        hasNextPage: remaining > 0))
+        next()
+    }
+    
+    /**
+     Shows a paged list of activities, sorted by date.
+     */
+    private func upcomingActivities(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        guard let pageString = request.queryParameters["page"],
+              let page = Int(pageString) else {
+            response.status(.badRequest)
+            return next()
+        }
+        let user: User?
+        if let userID = request.userProfile?.id {
+            guard let existingUser = try persistence.user(withID: userID) else {
+                throw log(ServerError.missingMiddleware(type: AuthenticationMiddleware.self))
+            }
+            user = existingUser
+        } else {
+            user = nil
+        }
+        let coordinates = user?.location?.coordinates ?? .default
+        let pageSize = 8
+        let activities = try persistence.upcomingActivities(notHostedBy: user,
+                                                            measuredFrom: coordinates,
+                                                            startingFrom: page * pageSize,
+                                                            limitedTo: pageSize)
+        let remaining = try persistence.numberOfActivities(notHostedBy: user) - (page + 1) * pageSize
+        let base = try baseViewModel(for: request)
+        try response.render("games-page", with: ActivitiesPageViewModel(base: base,
+                                                                        type: .upcoming,
+                                                                        activities: activities,
+                                                                        page: page,
+                                                                        hasNextPage: remaining > 0))
+        next()
+    }
+    
+    /**
+     Shows a paged list of activities, sorted by distance to the user.
+     */
+    private func activitiesNearMe(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws -> Void {
+        guard let pageString = request.queryParameters["page"],
+              let page = Int(pageString) else {
+            response.status(.badRequest)
+            return next()
+        }
+        let base = try baseViewModel(for: request)
+        guard let userID = request.userProfile?.id,
+              let user = try persistence.user(withID: userID),
+              user.location != nil else {
+            try response.render("games-page", with: ActivitiesPageViewModel(base: base,
+                                                                            type: .nearMe,
+                                                                            activities: [],
+                                                                            page: 0,
+                                                                            hasNextPage: false))
+            return next()
+        }
+        let pageSize = 8
+        let activities = try persistence.activitiesNear(user: user, startingFrom: page * pageSize, limitedTo: pageSize)
+        let remaining = try persistence.numberOfActivities(notHostedBy: user) - (page + 1) * pageSize
+        try response.render("games-page", with: ActivitiesPageViewModel(base: base,
+                                                                        type: .nearMe,
+                                                                        activities: activities,
+                                                                        page: page,
+                                                                        hasNextPage: remaining > 0))
         next()
     }
 }
---
+++ Sources/AroundTheTable/ViewModels/ActivitiesViewModel.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+/**
+ View model for **games.stencil**.
+ */
+struct ActivitiesViewModel: Codable {
+    
+    let base: BaseViewModel
+    
+    struct UserViewModel: Codable {
+        
+        let name: String
+        
+        init(_ user: User) {
+            self.name = user.name
+        }
+    }
+    
+    struct ActivityViewModel: Codable {
+        
+        let id: String
+        let host: UserViewModel
+        let name: String
+        let picture: String
+        let availableSeats: Int
+        let date: String
+        let location: Location
+        let distance: Int
+        
+        init(_ activity: Activity) throws {
+            guard let id = activity.id,
+                  let distance = activity.distance else {
+                throw log(ServerError.unpersistedEntity)
+            }
+            self.id = id.hexString
+            self.host = UserViewModel(activity.host)
+            self.name = activity.name
+            self.picture = activity.game?.picture?.absoluteString ?? Settings.defaultGamePicture
+            self.availableSeats = activity.availableSeats
+            self.date = activity.date.formatted(dateStyle: .full)
+            self.location = activity.location
+            self.distance = Int(ceil(distance / 1000)) // in km
+        }
+    }
+    
+    /// A selection of most recently added activities.
+    let newest: [ActivityViewModel]
+    
+    /// A selection of upcoming activities.
+    let upcoming: [ActivityViewModel]
+    
+    /// A selection of activities closest to the user.
+    let closest: [ActivityViewModel]
+    
+    /**
+     Initializes an `ActivitiesViewModel` with the given activities.
+     */
+    init(base: BaseViewModel, newest: [Activity], upcoming: [Activity], closest: [Activity]) throws {
+        self.base = base
+        self.newest = try newest.map(ActivityViewModel.init)
+        self.upcoming = try upcoming.map(ActivityViewModel.init)
+        self.closest = try closest.map(ActivityViewModel.init)
+    }
+}
