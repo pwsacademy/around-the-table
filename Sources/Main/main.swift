ab9a6970cdcba5d4971b82e1f9bcf497e2dd25e8
Steven Van Impe <steven.vanimpe@icloud.com>
2018-06-17 16:48:14 +0200

    Removed old files from Main and updated main.swift.

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -1,75 +1,15 @@
-import CloudFoundryEnv
+import AroundTheTable
 import Configuration
-import Foundation
 import HeliumLogger
 import Kitura
-import KituraSession
-import KituraStencil
-import LoggerAPI
-import MongoKitten
-import Stencil
 
-HeliumLogger.use()
+let persistence = try! Persistence()
+let router = Router()
+Routes(persistence: persistence).configure(using: router)
+
+HeliumLogger.use(.warning)
 
 let configuration = ConfigurationManager().load(.environmentVariables)
-
-private let router = Router()
-
-private let stencil = Extension()
-StencilFilters.register(on: stencil)
-router.setDefault(templateEngine: StencilTemplateEngine(extension: stencil))
-
-router.all(middleware: ForwardingMiddleware())
-
-router.all("/public", middleware: StaticFileServer())
-
-private let session = Session(secret: Secrets.sessionSecret, cookie: [.maxAge(14 * 24 * 3600.0)])
-
-router.all("/authentication", middleware: [session])
-private let credentials = configureAuthenticationRouter(using: router.route("/authentication"))
-
-router.all("/web", middleware: session)
-configureWebRouter(using: router.route("/web"), credentials)
-
-configureAdminRouter(using: router.route("/admin"))
-
-router.get("/") {
-    request, response, next in
-    try response.redirect("/web/home")
-    next()
-}
-
-/*
- Default error handler.
- Returns a 500 Internal Server Error for API requests.
- Renders an error page for web requests.
- */
-router.error {
-    request, response, next in
-    if let error = response.error {
-        let message: String
-        switch error {
-        case let error as Loggable:
-            message = error.message
-        default:
-            message = error.localizedDescription
-        }
-        if !(error is Loggable) {
-            // Loggable errors should have already been logged.
-            Log.error(message)
-        }
-        if request.originalURL.contains("/api") {
-            response.statusCode = .internalServerError
-            response.send(message)
-        } else {
-            try response.render("\(Settings.locale)/error", context: request.userInfo.merging([
-                "message": message
-            ]))
-        }
-        response.error = nil
-    }
-    next()
-}
-
 Kitura.addHTTPServer(onPort: configuration.port, with: router)
+print("Starting Kitura on port \(configuration.port)...")
 Kitura.run()
