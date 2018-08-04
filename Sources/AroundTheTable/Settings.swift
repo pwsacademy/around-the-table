56c7e58a19c9b4a05133368f8daec412da515dca
Steven Van Impe <steven.vanimpe@icloud.com>
2018-08-04 23:28:48 +0200

    Settings now uses tuples instead of nested enums.

--- Sources/AroundTheTable/Settings.swift
+++ Sources/AroundTheTable/Settings.swift
@@ -41,44 +41,43 @@ enum Settings {
     /**
      Settings related to Cloud Object Storage.
      */
-    enum cloudObjectStorage {
+    static let cloudObjectStorage = (
         
         /// The API key that should be used to request an OAuth token.
         /// This setting is optional and should be provided by an environment variable.
         /// If no API key is provided, cloud object storage will not be used.
-        static let apiKey = settings["ATT:COS:API_KEY"] as? String
+        apiKey: settings["ATT:COS:API_KEY"] as? String,
         
         /// The complete URL of the bucket where the images should be stored.
-        static let bucketURL = settings["ATT:COS:BUCKET_URL"] as? String
-    }
+        bucketURL: settings["ATT:COS:BUCKET_URL"] as? String
+    )
     
     /**
      Database configuration.
      */
-    enum database {
+    static let database = (
         
         /// The name by which the Compose for MongoDB service is connected.
         /// This setting is optional and is only used when running on Bluemix.
         /// When it is defined, it replaces **ATT.DATABASE.URI**.
-        static let service = settings["ATT:DATABASE:SERVICE"] as? String
+        service: settings["ATT:DATABASE:SERVICE"] as? String,
         
         /// The URI of the MongoDB server.
-        static let uri = settings["ATT:DATABASE:URI"] as! String
+        uri: settings["ATT:DATABASE:URI"] as! String,
         
         /// The name of the database.
-        static let name = settings["ATT:DATABASE:NAME"] as! String
-    }
+        name: settings["ATT:DATABASE:NAME"] as! String
+    )
     
     /**
      A default location that is used to calculate distances when a user has not specified his/her location.
      
      This location is also available as `Coordinates.default`.
      */
-    enum defaultCoordinates {
-        
-        static let latitude = settings["ATT:DEFAULT_COORDINATES:LATITUDE"] as! Double
-        static let longitude = settings["ATT:DEFAULT_COORDINATES:LONGITUDE"] as! Double
-    }
+    static let defaultCoordinates = (
+        latitude: settings["ATT:DEFAULT_COORDINATES:LATITUDE"] as! Double,
+        longitude: settings["ATT:DEFAULT_COORDINATES:LONGITUDE"] as! Double
+    )
 
     /// The default picture for a game.
     static let defaultGamePicture = settings["ATT:DEFAULT_GAME_PICTURE"] as! String
@@ -92,30 +91,30 @@ enum Settings {
     /**
      Settings related to Facebook Web Login.
      */
-    enum facebook {
+    static let facebook = (
         
         /// The Facebook app ID.
-        static let app = settings["ATT:FACEBOOK:APP"] as! String
+        app: settings["ATT:FACEBOOK:APP"] as! String,
         
         /// The Facebook app secret.
         /// This should be provided by an environment variable.
-        static let secret = settings["ATT:FACEBOOK:SECRET"] as! String
-    }
+        secret: settings["ATT:FACEBOOK:SECRET"] as! String
+    )
     
     /**
      Settings related to Google.
      */
-    enum google {
+    static let google = (
         
         /// The countries to which you want to limit Google Maps address autocompletion results.
-        static let countries = settings["ATT:GOOGLE:COUNTRIES"] as! [String]
+        countries: settings["ATT:GOOGLE:COUNTRIES"] as! [String],
         
         /// The Google Maps API key.
         /// Note that this isn't really a secret, as it is visible in the source code.
         /// Hence, it should be limited to your domain so it can't be misused.
         /// A default value is provided in **settings.json** for use during development.
-        static let secret = settings["ATT:GOOGLE:SECRET"] as! String
-    }
+        secret: settings["ATT:GOOGLE:SECRET"] as! String
+    )
     
     /// The locale used to localize anything that needs it (e.g. views, messages, dates, ...).
     static let locale = settings["ATT:LOCALE"] as! String
