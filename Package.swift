9926c0515fd2edfa70b3fd509a161fb1bc0c4f6b
Steven Van Impe <steven.vanimpe@icloud.com>
2018-08-25 17:17:13 +0200

    Temporarily fork Stencil to fix a performance problem.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.4.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git", .upToNextMinor(from: "2.2.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura-Session.git", .upToNextMinor(from: "3.2.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.10.0")),
+        .package(url: "https://github.com/svanimpe/Kitura-StencilTemplateEngine.git", .branch("template-patch")),
         .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMinor(from: "6.0.0")),
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", .upToNextMinor(from: "4.1.0")),
         .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMinor(from: "1.1.0")),
