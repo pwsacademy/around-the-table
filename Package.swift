107d2abba9163756f6d7140e344e652119e4a78f
Steven Van Impe <steven.vanimpe@icloud.com>
2019-08-06 18:17:10 +0200

    Update versions for Swift 5.

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "AroundTheTable",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Health.git", .upToNextMinor(from: "1.0.0")),
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.8.0")),
-        .package(url: "https://github.com/IBM-Swift/swift-html-entities", .upToNextMinor(from: "3.0.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.6.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git", .upToNextMinor(from: "2.2.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-Session.git", .upToNextMinor(from: "3.3.0")),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.11.0")),
-        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", .upToNextMinor(from: "6.0.0")),
-        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .upToNextMinor(from: "4.1.0")),
-        .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", .upToNextMinor(from: "2.0.0")),
+        .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.9.0"),
+        .package(url: "https://github.com/IBM-Swift/swift-html-entities", from: "3.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.7.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git", from: "2.2.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-Session.git", from: "3.3.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.11.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "6.0.0"),
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .branch("master/4.1")),
+        .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", from: "2.1.0"),
     ],
     targets: [
         .target(name: "Main", dependencies: [
